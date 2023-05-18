e9d07279dcf95957968ce28d6638493f0d118133
phemsie <femi_oworu@yahoo.com>
2023-05-18 18:49:50 +0000

    Application server

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -8,7 +8,7 @@ from flask import Flask
 app = Flask("__name__")
 
 
-@app.route("airbnb-onepage", strict_slashes=False)
+@app.route('/airbnb-onepage/', strict_slashes=False)
 def hello():
     """Return a given string"""
     return "Hello HBNB!"
